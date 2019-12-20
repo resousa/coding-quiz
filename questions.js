4d517c63bb83b8b232452d073e51238ffdf7ac27
Ryan Sousa <Sousa.Ryan4@gmail.com>
2019-12-19 23:47:31 -0800

    added new files

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questions = [
+
+    {
+        title: 'Question 1?',
+        choices: ['wrong', "wrong", "right", "wrong"],
+        answer: 'right',
+    },
+    
+    {
+        title: 'Question 2?',
+        choices: ['wrong', "wrong", "right", "wrong"],
+        answer: 'right',
+    },
+
+    {
+        title: 'Question 3?',
+        choices: ['wrong', "wrong", "right", "wrong"],
+        answer: 'right',
+    },
+
+    {
+        title: 'Question 4?',
+        choices: ['wrong', "wrong", "right", "wrong"],
+        answer: 'right',
+    },
+
+    {
+        title: 'Question 5?',
+        choices: ['wrong', "wrong", "right", "wrong"],
+        answer: 'right',
+    },
+
+    {
+        title: 'Question 6?',
+        choices: ['wrong', "wrong", "right", "wrong"],
+        answer: 'right',
+    },
+]
