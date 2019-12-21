ff1e6a31a06e038ec60c6e901d4b5f243364c45b
Ryan Sousa <Sousa.Ryan4@gmail.com>
2019-12-21 07:50:26 -0800

    questions working

--- questions.js
+++ questions.js
@@ -1,37 +1,37 @@
 var questions = [
   {
     title: "Question 1?",
-    choices: ["wrong", "wrong", "right", "wrong"],
-    answer: "right"
+    choices: ["ronwg1", "right", "wrong3", "wrong4"],
+    answer: "2"
   },
 
   {
     title: "Question 2?",
-    choices: ["wrong", "wrong", "right", "wrong"],
-    answer: "right"
+    choices: ["wrongg1", "wrongg2", "right", "wrongg4"],
+    answer: "3"
   },
 
   {
     title: "Question 3?",
-    choices: ["wrong", "wrong", "right", "wrong"],
-    answer: "right"
+    choices: ["right", "wrondsg2", "wrong3", "4wrong"],
+    answer: "1"
   },
 
   {
     title: "Question 4?",
-    choices: ["wrong", "wrong", "right", "wrong"],
-    answer: "right"
+    choices: ["wrooo1ng", "wr0002ong", "right", "wron4g"],
+    answer: "3"
   },
 
   {
     title: "Question 5?",
-    choices: ["wrong", "wrong", "right", "wrong"],
-    answer: "right"
+    choices: ["1wrong", "2wrong", "3wrong", "right"],
+    answer: "4"
   },
 
   {
     title: "Question 6?",
     choices: ["wrong", "wrong", "right", "wrong"],
-    answer: "right"
+    answer: "3"
   }
 ];
--- script.js
+++ script.js
@@ -1,16 +1,85 @@
-$(document).ready(function() {
-  /// Intro screen
-  $("#introHead").text("The Coding Quiz!");
-  $("#introBody").text(
-    "The Coding Quiz is a multiple choice game. You will have 90 seconds to finish. Every answer that is incorrect will deduct 15 seconds from the clock. Your score will be determined by the amount of time left on the clock. Are you ready for a challenge?"
-  );
-  $("#start").text("I'm Ready!");
+/// Intro screen
+$("#introHead").text("The Coding Quiz!");
+$("#introBody").text(
+  "The Coding Quiz is a multiple choice game. You will have 90 seconds to finish. Every answer that is incorrect will deduct 15 seconds from the clock. Your score will be determined by the amount of time left on the clock. Are you ready for a challenge?"
+);
+$("#start").text("I'm Ready!");
 
-  /// Click function on start button
-  $("#start").on("click", startQuiz);
+/// Click function on start button
+$("#start").on("click", startQuiz);
 
-  /// Start quiz
-  function startQuiz() {
-    $(".content").empty();
-  }
+/// Start quiz
+function startQuiz() {
+  $("#intro").empty();
+  questionStart();
+  $("#quiz").show();
+  timerFunction();
+  timer = setInterval(timerFunction, 1000);
+}
+
+///Question gen
+var questionCount = 0;
+
+function questionStart() {
+  $("#question").text(questions[questionCount].title);
+  $("#answer1").text(questions[questionCount].choices[0]);
+  $("#answer2").text(questions[questionCount].choices[1]);
+  $("#answer3").text(questions[questionCount].choices[2]);
+  $("#answer4").text(questions[questionCount].choices[3]);
+}
+
+/// Answer check
+$("#answer1").on("click", function() {
+  checkFunction("1");
 });
+$("#answer2").on("click", function() {
+  checkFunction("2");
+});
+$("#answer3").on("click", function() {
+  checkFunction("3");
+});
+$("#answer4").on("click", function() {
+  checkFunction("4");
+});
+
+function checkFunction(input) {
+  if (input === questions[questionCount].answer) {
+    $("#answer").text("Nice Work!");
+  } else {
+    $("#answer").text("Better luck next question");
+    time = time - 15;
+  }
+
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    questionStart();
+  } else {
+    clearInterval(timer);
+    $("#quiz").empty();
+    $(".timer").empty();
+    $("#result").show();
+    showScore(time);
+  }
+}
+
+///Timer gen
+var time = 90;
+var timer;
+
+function timerFunction() {
+  if (time > 0) {
+    $("#counter").text(time);
+    time--;
+  } else {
+    clearInterval(timer);
+    $("#quiz").empty();
+    $(".timer").empty();
+    $("#result").show();
+    showScore(time);
+  }
+}
+function showScore(time) {
+  $("#finishHead").text("Finished!");
+  $("#finishScore").text("Final score: " + time);
+  $("#submit").text("See where you rank!");
+}
