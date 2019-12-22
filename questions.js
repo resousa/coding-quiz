ea00ffe9ca41e286336523295cfb41bc2d5bf521
Ryan Sousa <Sousa.Ryan4@gmail.com>
2019-12-22 00:06:47 -0800

    update questions

--- questions.js
+++ questions.js
@@ -1,37 +1,37 @@
 var questions = [
   {
-    title: "Question 1?",
-    choices: ["ronwg1", "right", "wrong3", "wrong4"],
-    answer: "2"
-  },
-
-  {
-    title: "Question 2?",
-    choices: ["wrongg1", "wrongg2", "right", "wrongg4"],
-    answer: "3"
-  },
-
-  {
-    title: "Question 3?",
-    choices: ["right", "wrondsg2", "wrong3", "4wrong"],
-    answer: "1"
-  },
-
-  {
-    title: "Question 4?",
-    choices: ["wrooo1ng", "wr0002ong", "right", "wron4g"],
-    answer: "3"
-  },
-
-  {
-    title: "Question 5?",
-    choices: ["1wrong", "2wrong", "3wrong", "right"],
+    title: "what does api stand for?",
+    choices: ["artificial program interface", "artificial program index" , "application program index", "application program interface"],
     answer: "4"
   },
 
   {
-    title: "Question 6?",
-    choices: ["wrong", "wrong", "right", "wrong"],
+    title: "Choose the largest heading size",
+    choices: ["<h0>", "<h1>", "<h5>", "<h10>"],
+    answer: "2"
+  },
+
+  {
+    title: "How many columns does the bootstarp grid system have?",
+    choices: ["6", "10", "12", "15"],
     answer: "3"
+  },
+
+  {
+    title: "How would you clear a div with an id value of quiz with jQuery?",
+    choices: ["$('.quiz').clear()", "$clear('#quiz')", "$('#quiz').empty();", "$('.quiz').clear()"],
+    answer: "3"
+  },
+
+  {
+    title: "how would you flex an object to the far right side of the screen with display: flex;?",
+    choices: ["align-items: baseline;", "justify-content: flex-right;", "align-items: flex-end;", "justify-content: flex-end;"],
+    answer: "4"
+  },
+
+  {
+    title: "Which of these JavaScript functions returns an integer of a specific base",
+    choices: ["parseInt()", "num()", "parseNum()", "numVal()"],
+    answer: "1"
   }
 ];
